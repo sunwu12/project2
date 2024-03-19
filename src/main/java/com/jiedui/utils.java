adc9adbb9d88faf254c49e0a434ec6c0e2fbedc5
sun <2560755968@qq.com>
2024-03-19 15:40:01 +0800

    添加约分

--- src/main/java/com/jiedui/utils.java
+++ src/main/java/com/jiedui/utils.java
@@ -36,30 +36,43 @@ public class utils {
         System.out.println(s);
         return s;
     }
+    /*
+    计算公约数用于约分
+     */
+    public static int calculateGCD(int x, int y){
+        if(x%y==0){
+            return y;
+        }
+        return calculateGCD(y,x%y);
+    }
+
 
     public static String GPF(int x, int y) {
 
-        if (x < y) {
-            StringBuilder outcom = new StringBuilder();
-            outcom.append(x);
-            outcom.append("/");
-            outcom.append(y);
-            return outcom.toString();
+        if(x%y==0)
+        {
+            return String.valueOf(x/y);
+        }
+        else {
+            int gcd=calculateGCD(x,y);
+            x/=gcd;
+            y/=gcd;
+            if (x < y) {
+                return x + "/" + y;
 
-        } else if (x > y) {
-            int a = x / y;
-            int b = (x % y);
-            StringBuilder outcom1 = new StringBuilder();
-            String A = String.valueOf(a);
-            String B = String.valueOf(b);
-            outcom1.append(A);
-            outcom1.append("'");
-            outcom1.append(B);
-            outcom1.append("/");
-            outcom1.append(y);
-            return outcom1.toString();
-
-
+            } else if (x > y) {
+                int a = x / y;
+                int b = (x % y);
+                StringBuilder outcom2 = new StringBuilder();
+                String A = String.valueOf(a);
+                String B = String.valueOf(b);
+                outcom2.append(A);
+                outcom2.append("'");
+                outcom2.append(B);
+                outcom2.append("/");
+                outcom2.append(y);
+                return outcom2.toString();
+            }
         }
         return null;
     }
