7ce8ff552cfd580a9bfa9e96be9b47f3ef5092c8
sun <2560755968@qq.com>
2024-03-19 15:02:21 +0800

    1

--- src/main/java/com/jiedui/utils.java
+++ src/main/java/com/jiedui/utils.java
@@ -1,40 +1,18 @@
 package com.jiedui;
 
-import java.util.Random;
-
 public class utils {
-    public static String getRandomSign(int count,int maxValue){
-        char[] chars = {'+','-','×','÷'};
-        Random rand = new Random();
-        int a=rand.nextInt(maxValue);
-        int b=rand.nextInt(maxValue);
-        int result=0;
-        char sign = chars[rand.nextInt(4)];
-        switch (sign){
-            case '+':result=a+b;break;
-            case '-':result=a-b;break;
-            case '×':result=a*b;break;
-            case '÷':result=a/b;break;
-        }
-        String s=a+" "+sign+" "+b+" = "+result;
-        System.out.println(s);
-        return s;
-    }
+    public static String GPF(int x, int y) {
 
-
-    public static String getProperFration(String x, String y) {
-        int num1 = Integer.parseInt(x);
-        int num2 = Integer.parseInt(y);
-        if (num1 < num2) {
+        if (x < y) {
             StringBuilder outcom = new StringBuilder();
             outcom.append(x);
             outcom.append("/");
             outcom.append(y);
             return outcom.toString();
 
-        } else if (num1 > num2) {
-            int a = num1 / num2;
-            int b = (num1 % num2);
+        } else if (x > y) {
+            int a = x / y;
+            int b = (x % y);
             StringBuilder outcom1 = new StringBuilder();
             String A = String.valueOf(a);
             String B = String.valueOf(b);
@@ -49,5 +27,4 @@ public class utils {
         }
         return null;
     }
-
 }
