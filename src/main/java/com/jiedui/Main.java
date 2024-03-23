019e408f903309deb985c38fd7105b5334b3ac3e
sun <2560755968@qq.com>
2024-03-23 18:48:13 +0800

    完成了终端的输入

--- src/main/java/com/jiedui/Main.java
+++ src/main/java/com/jiedui/Main.java
@@ -7,17 +7,17 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int count=0,maxValue=0;
-        System.out.println("22222222");
- String subjectPath=null,answerPath=null;
- for(int i=0;i<args.length;i++){
- if(args[i].equals("-n")){
- count=Integer.parseInt(args[i+1]);
- }else if(args[i].equals("-r"))
- {
- maxValue=Integer.parseInt(args[i+1]);
- }else if(args[i].equals("-e")){
- subjectPath=args[i+1];
+         int count=0,maxValue=0;
+         System.out.println("123456");
+         String subjectPath=null,answerPath=null;
+         for(int i=0;i<args.length;i++){
+        if(args[i].equals("-n")){
+         count=Integer.parseInt(args[i+1]);
+        }else if(args[i].equals("-r"))
+        {
+          maxValue=Integer.parseInt(args[i+1]);
+       }else if(args[i].equals("-e")){
+  subjectPath=args[i+1];
 
  }else if(args[i].equals("-a")){
  answerPath=args[i+1];
@@ -28,9 +28,9 @@ public class Main {
  System.out.println(e);
  }
  if(count!=0&&maxValue!=0) {
- TxtHandle.txtRecord(es);
+ TxtHandle.txtRecord(es,"src\\resources\\Exercises.txt","src\\resources\\Answers.txt");
  }else if(subjectPath!=null&&answerPath!=null){
- TxtHandle.txtJudge(subjectPath,answerPath);
+ TxtHandle.txtJudge(subjectPath,answerPath,"src\\resources\\Grade.txt");
  }
 
     }
--- src/main/java/com/jiedui/TxtHandle.java
+++ src/main/java/com/jiedui/TxtHandle.java
@@ -2,6 +2,7 @@ package com.jiedui;
 
 import cn.hutool.core.io.FileUtil;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -10,56 +11,59 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class TxtHandle {
-    public static void txtRecord(List<Expression> es){
-        String subjectPath="D:\\codes\\jiedui\\src\\resources\\Exercises.txt";
-        String answerPath="D:\\codes\\jiedui\\src\\resources\\Answers.txt";
-        AtomicInteger i = new AtomicInteger();
-        AtomicInteger j = new AtomicInteger();
-        List<String> a=FileUtil.readUtf8Lines(subjectPath);
-        if(a.isEmpty()) {
-            i.set(1);
-            j.set(1);
-        } else {
-            String str=a.get(a.size()-1);
-            i.set(Integer.parseInt(str.split("\\.")[0])+1);
-            j.set(Integer.parseInt(str.split("\\.")[0])+1);
-        }
+    public static void txtRecord(List<Expression> es, String subjectPath,String answerPath){
 
-        List<String> subjectList=es.stream().map(e->i.getAndIncrement()+". "+e.expression+" = ").collect(Collectors.toList());
-        List<String> answerList=es.stream().map(e->j.getAndIncrement()+". "+e.value).collect(Collectors.toList());
-
-        FileUtil.appendUtf8Lines(subjectList,subjectPath);
-        FileUtil.appendUtf8Lines(answerList,answerPath);
-    }
-
-    public static void txtJudge(String subjectPath,String answerPath){
-        List<String> subjectList=FileUtil.readUtf8Lines(subjectPath);
-        List<String> anwerList=FileUtil.readUtf8Lines(answerPath);
-        List<String> expList=subjectList.stream().map(s->s.replace(" =","")
-                .split("^\\d+\\.")[1]).collect(Collectors.toList());
-        List<String> valList=anwerList.stream().map(s->s.replace(" =","")
-                .split("^\\d+\\.")[1].trim()).collect(Collectors.toList());
-        int[] gradeList=new int[subjectList.size()];
-        for(int i=0;i<subjectList.size();i++){
-            gradeList[i]= Objects.equals(utils.cal(expList.get(i)), valList.get(i)) ?1:0;
-        }
-        StringBuilder sb1=new StringBuilder();
-        StringBuilder sb2=new StringBuilder();
-        long corNum=Arrays.stream(gradeList).filter(i->i==1).count();
-        long wroNum=Arrays.stream(gradeList).filter(i->i==0).count();
-        sb1.append("Correct: ").append(corNum).append("(");
-        sb2.append("Wrong: ").append(wroNum).append("(");
-        for(int i=0;i<subjectList.size();i++){
-            if(gradeList[i]==1) {
-                sb1.append(i + 1).append(",");
+            AtomicInteger i = new AtomicInteger();
+            AtomicInteger j = new AtomicInteger();
+        File f1=FileUtil.file(subjectPath);
+            List<String> a=FileUtil.readUtf8Lines(f1);
+            if(a.isEmpty()) {
+                i.set(1);
+                j.set(1);
+            } else {
+                String str=a.get(a.size()-1);
+                i.set(Integer.parseInt(str.split("\\.")[0])+1);
+                j.set(Integer.parseInt(str.split("\\.")[0])+1);
             }
-            else sb2.append(i+1).append(",");
+
+            List<String> subjectList=es.stream().map(e->i.getAndIncrement()+". "+e.expression+" = ").collect(Collectors.toList());
+            List<String> answerList=es.stream().map(e->j.getAndIncrement()+". "+e.value).collect(Collectors.toList());
+
+
+        File f2=FileUtil.file(answerPath);
+            FileUtil.appendUtf8Lines(subjectList,f1);
+            FileUtil.appendUtf8Lines(answerList,f2);
         }
-        if(sb1.charAt(sb1.length()-1)==',')sb1.deleteCharAt(sb1.length()-1);
-        if(sb2.charAt(sb2.length()-1)==',')sb2.deleteCharAt(sb2.length()-1);
-        sb1.append(")");
-        sb2.append(")");
-        String gradePath="D:\\codes\\jiedui\\src\\resources\\Grade.txt";
-        FileUtil.writeUtf8Lines(new ArrayList<>(List.of(new String[]{sb1.toString(),sb2.toString()})), gradePath);
-    }
-}
+
+        public static void txtJudge(String subjectPath,String answerPath,String gradePath){
+
+                List<String> subjectList=FileUtil.readUtf8Lines(subjectPath);
+                List<String> anwerList=FileUtil.readUtf8Lines(answerPath);
+                List<String> expList=subjectList.stream().map(s->s.replace(" =","")
+                        .split("^\\d+\\.")[1]).collect(Collectors.toList());
+                List<String> valList=anwerList.stream().map(s->s.replace(" =","")
+                        .split("^\\d+\\.")[1].trim()).collect(Collectors.toList());
+                int[] gradeList=new int[subjectList.size()];
+                for(int i=0;i<subjectList.size();i++){
+                    gradeList[i]= Objects.equals(utils.cal(expList.get(i)), valList.get(i)) ?1:0;
+                }
+                StringBuilder sb1=new StringBuilder();
+                StringBuilder sb2=new StringBuilder();
+                long corNum=Arrays.stream(gradeList).filter(i->i==1).count();
+                long wroNum=Arrays.stream(gradeList).filter(i->i==0).count();
+                sb1.append("Correct: ").append(corNum).append("(");
+                sb2.append("Wrong: ").append(wroNum).append("(");
+                for(int i=0;i<subjectList.size();i++){
+                    if(gradeList[i]==1) {
+                        sb1.append(i + 1).append(",");
+                    }
+                    else sb2.append(i+1).append(",");
+                }
+                if(sb1.charAt(sb1.length()-1)==',')sb1.deleteCharAt(sb1.length()-1);
+                if(sb2.charAt(sb2.length()-1)==',')sb2.deleteCharAt(sb2.length()-1);
+                sb1.append(")");
+                sb2.append(")");
+                gradePath="src\\resources\\Grade.txt";
+                FileUtil.writeUtf8Lines(new ArrayList<>(List.of(new String[]{sb1.toString(),sb2.toString()})), gradePath);
+            }
+        }
